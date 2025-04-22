73a611c7912faf0698fd9ba6b4cc53cee22a6eb9
nithe-C5i <nithesh.a@c5i.ai>
2025-04-22 13:08:41 +0530

    Backend Port Update

--- src/pages/TryDashboard.tsx
+++ src/pages/TryDashboard.tsx
@@ -197,7 +197,7 @@ const Dashboard: React.FC = () => {
   const fetchDashboardData = async () => {
     try {
       setLoading(true)
-      const response = await axios.get("http://172.203.229.218:8080/dashboard-data", {
+      const response = await axios.get("http://172.203.229.218:8082/dashboard-data", {
         
         params: {
           modality: modality === "All" ? undefined : modality,
@@ -220,7 +220,7 @@ const Dashboard: React.FC = () => {
   const fetchTableData = async () => {
     setTableLoading(true)
     try {
-      const response = await axios.get("http://172.203.229.218:8080/table-data", {
+      const response = await axios.get("http://172.203.229.218:8082/table-data", {
         params: {
           modality: modality === "All" ? undefined : modality,
           complexity: complexity === "All" ? undefined : complexity === "Medium" ? "Intermediate" : complexity,
